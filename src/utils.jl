0333c269386423c28cff78cae64005fd910eca7f
patrickm663 <rev0cation2025@outlook.com>
2022-11-01 23:00:44 +0200

    Update notebook and remove claim-prediction.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,11 @@
+module utils
+
+export lowVarianceFilter, dummyEncode, dummyEncodeAll, minMaxScale, confusionMatrix, metrics 
+
 using Statistics
 using DataFrames
-using Optim
 
-function LowVarianceFilter(X; t=0.001)
+function lowVarianceFilter(X; t=0.001)
 ## Purpose: drops columns with low/zero variance
 ## Input: A Matrix (DataFrame untested), a threshhold t (default=0.001)
 ## Output: A Matrix (DataFrame untested)
@@ -15,14 +18,14 @@ function LowVarianceFilter(X; t=0.001)
     return X[:, (1:end) .∉ (l,)] 
 end
 
-function DummyEncodeAll(X::DataFrame)::DataFrame
+function dummyEncodeAll(X)
 ## Purpose: dummy encodes all String columns in a DataFrame
 ## Input: A DataFrame
 ## Output: A DataFrame
     df = DataFrame()
     for i ∈ 1:size(X, 2)
-        if string(typeof(data[1, i])) ∉ ["Float16", "Float32", "Float64", "Int8", "Int16", "Int32", "Int64"]
-            de = DummyEncode(X[!, i], names(X)[i])
+        if string(typeof(X[1, i])) ∉ ["Float16", "Float32", "Float64", "Int8", "Int16", "Int32", "Int64"]
+            de = dummyEncode(X[!, i], names(X)[i])
             for c ∈ names(de)
                 df[!, c] = de[!, c]
             end
@@ -33,7 +36,7 @@ function DummyEncodeAll(X::DataFrame)::DataFrame
     return df
 end
 
-function DummyEncode(x, name)::DataFrame
+function dummyEncode(x, name)
 ## Purpose: dummy encodes a given vector, with the first entry dropped 
 ## as a baseline.
 ## Input: vector of Strings and a name to assign subseqent columns
@@ -48,7 +51,7 @@ function DummyEncode(x, name)::DataFrame
     return df[!, (1:end) .!= 1]
 end
 
-function MinMaxScale(x::Vector)::Vector
+function minMaxScale(x::Vector)::Vector
 ## Purpose: Scales a vector of inputs to between zero and one
 ## Input: a vector
 ## Output: a vector
@@ -56,67 +59,28 @@ function MinMaxScale(x::Vector)::Vector
     return x
 end
 
-function YJ(y::Vector, λ)::Vector
-## Purpose: Performs the Yeo-Johnson power transformation
-## Input: a vector and λ
-## Output: a vector
-    for i in 1:length(y)
-        if y[i] ≥ 0
-            if λ ≠ 0
-                y[i] = ((y[i] + 1)^λ - 1)/λ
-            else
-                y[i] = log(y[i] + 1)
-            end
-        else
-            if λ ≠ 2
-                y[i] = -((-y[i] + 1)^(2 - λ) - 1)/(2 - λ)
-            else
-                y[i] = -log(-y[i] + 1)
-            end
+function confusionMatrix(y, ŷ)::Matrix
+## Purpose: Generates a confusion matrix for binary target variables
+## Input: A vector of actuals and a vector of predictions
+## Outpus: Returns a 2x2 matrix
+    C = zeros(2, 2)
+    for i ∈ 0:1
+        for j ∈ 0:1
+            C[i+1, j+1] = sum((y .== i) .& (ŷ .== j))
         end
     end
-    return y
+    return C
 end
 
-function YeoJohnson(y::Vector; min = -2, max = 2, λ = 0.1, opt = true)::Vector
-## Purpose: Calls the YJ function depending on whether lambda should be optimised or not
-## Input: A Vector. Optional inputs are the min and max search range for the optimiser, an optional λ parameter, and a flag for whether to optimise or use the λ input provided
-## Output: a Vector of Yeo-Johnson transformed values
-    if opt == true
-        λ = λoptimum(y, min, max)
-        return YJ(y, λ) 
-    else
-        return YJ(y, λ)
-    end
+function metrics(C::Matrix)
+## Purpose: Returns a tuple of standard performance metrics for binary classification using a confusion matrix
+## Input: A 2x2 confusion matrix
+## Output: A named tuple
+    accuracy = (C[1, 1]+C[2, 2])/(C[1, 1]+C[1, 2]+C[2, 1]+C[2, 2])
+    precision = C[1, 1]/(C[1, 1]+C[2, 1])
+    recall = C[1, 1]/(C[1, 1]+C[1, 2])
+    F1_score = 2*(precision*recall)/(precision+recall)
+    return (Accuracy = accuracy, Precision = precision, Recall = recall, F1_Score = F1_score)
 end
 
-function LogLike(y, λ)::Float32
-## Purpose: Computes the log-likelihood of the Yeo-Johnson power transformation
-## Input: a Vector and λ parameter
-## Output: a Float
-## Source: Algorithm from SciPy.stats `yeojohnson_lif` function in _morestats.py
-    N = length(y)
-    σ̂² = var(YJ(y, λ))
-    LL = -N/2 * log(σ̂²) + (λ - 1) * sum(sgn.(y) .* log.(abs.(y) .+ 1))
-    return LL
-end
-
-function sgn(x)
-## Purpose: Calculates the "sign" of a number (1 if positive, -1 if negative, zero otherwise)
-## Input: a number
-## Output: 1, -1, or 0
-    if x ≠ 0.0
-        return x/abs(x)
-    else
-        return 0
-    end
-end
-
-function λoptimum(y::Vector, min, max)::Float32
-## Purpose: Computes the value of λ that minimises the log-likelihood of the Yeo-Johnson power transformation
-## Input: a Vector and minimum and maximum values for the search range
-## Output: a Value for λ̂, the minimiser
-    LL(λ) = LogLike(y, λ)
-    λ̂ = optimize(LL, min, max).minimizer
-    return λ̂
-end
+end # module
